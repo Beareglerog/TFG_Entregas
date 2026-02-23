f9f81e7027678d588fcbaa56b123506728c732c7
Beareglerog <beareglero@gmail.com>
2026-02-22 18:42:38 -0800

    Corrigiendo errores

--- main.py
+++ main.py
@@ -71,51 +71,18 @@ def run_demo(inputs):
     ACS_SPF = float(str(lookup_value(tabla_ACSspf, fila_ACS_SPF, inst_acs)).replace(",", ".")) 
     consumo_ACS = demanda_ACS / ACS_SPF 
     ##habia discrepancia entre consumo_ACS y consumo_acs
-    consumo_gnACS = consumo_gas(tipo_calefaccion, tipo_acs, consumo_calefaccion, consumo_ACS, inst_calefaccion, inst_acs, 'ACS') 
-    coste_fijoACS = tarifa_suministro(inst_acs, tipo_acs, consumo_gnACS, 'fijo', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
-    coste_fijoACSreparto = coste_fijoacsreparto(inst_acs, tipo_acs, inst_calefaccion, tipo_calefaccion, coste_fijoACS) 
-    coste_variableACS = tarifa_suministro(inst_acs, tipo_acs, consumo_gnACS, 'variable', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
-    alquiler_equiposACS = alquiler_equiposacs(tipo_acs, inst_acs) 
-    impuesto_gasACS = impuesto_gas(tipo_acs) 
-    impuesto_electricidadACS = impuesto_electricidad(tipo_acs) 
-    gasto_ACS = (coste_fijoACSreparto*12 + coste_variableACS*consumo_ACS + alquiler_equiposACS*12 + impuesto_gasACS*consumo_ACS + impuesto_electricidadACS*coste_variableACS*consumo_ACS)*(1+factor_ivaacs(tipo_acs, provincia)/100) 
-    gasto_ACSsinimp = coste_fijoACSreparto*12 + coste_variableACS*consumo_ACS 
+   # consumo_gnACS = consumo_gas(tipo_calefaccion, tipo_acs, consumo_calefaccion, consumo_ACS, inst_calefaccion, inst_acs, 'ACS') 
+   ######## coste_fijoACS = tarifa_suministro(inst_acs, tipo_acs, consumo_gnACS, 'fijo', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
+   ####### coste_fijoACSreparto = coste_fijoacsreparto(inst_acs, tipo_acs, inst_calefaccion, tipo_calefaccion, coste_fijoACS) 
+   ###### coste_variableACS = tarifa_suministro(inst_acs, tipo_acs, consumo_gnACS, 'variable', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
+   ##### alquiler_equiposACS = alquiler_equiposacs(tipo_acs, inst_acs) 
+   #### impuesto_gasACS = impuesto_gas(tipo_acs) 
+   ### impuesto_electricidadACS = impuesto_electricidad(tipo_acs) 
+  ##  gasto_ACS = (coste_fijoACSreparto*12 + coste_variableACS*consumo_ACS + alquiler_equiposACS*12 + impuesto_gasACS*consumo_ACS + impuesto_electricidadACS*coste_variableACS*consumo_ACS)*(1+factor_ivaacs(tipo_acs, provincia)/100) 
+  #  gasto_ACSsinimp = coste_fijoACSreparto*12 + coste_variableACS*consumo_ACS 
 
-    # CALEFACCIÓN
-    zona_invierno = zonainv(provincia, altitud) 
-    C1 = calcula_c1(califE, zona_invierno, tipo_vivienda) 
-    Io_Is = io_is(zona_invierno, tipo_vivienda, C1) 
-    demanda_RefCal = demanda_referenciacalefaccion(provincia, altitud, tipo_vivienda) 
-    demanda_CorregidaSigno = Io_Is * demanda_RefCal * superficie 
-    demanda_CorregidaCal = fdemanda_corregidacal(demanda_CorregidaSigno) 
-    HSPF = float(str(hspf(tipo_calefaccion, inst_calefaccion, zona_invierno, califE)).replace(",", "."))
-    consumo_calefaccion = demanda_CorregidaCal / HSPF 
-    # TODAVIA NO HE DEFINIDO DEMANDA ACS
-    consumo_acs = demanda_ACS / ACS_SPF 
-    ##habia discrepancia entre consumo_ACS y consumo_acs
-    consumo_gnCAL = consumo_gas(tipo_calefaccion, tipo_acs, consumo_calefaccion, consumo_acs, inst_calefaccion, inst_acs, 'calefaccion') 
-    coste_fijoCAL = tarifa_suministro(inst_calefaccion, tipo_calefaccion, consumo_gnCAL, 'fijo', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
-    coste_fijoCALreparto = coste_fijocalreparto(tipo_calefaccion, inst_calefaccion, coste_fijoCAL) 
-    coste_variableCAL = tarifa_suministro(inst_calefaccion, tipo_calefaccion, consumo_gnCAL, 'variable', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
-    alquiler_equiposCAL = alquiler_equipos(tipo_calefaccion) 
-    impuesto_gas = impuesto_gas(tipo_calefaccion) 
-    impuesto_electricidad = impuesto_electricidad(tipo_calefaccion) 
-    gasto_CAL = (coste_fijoCALreparto*12 + coste_variableCAL*consumo_calefaccion + alquiler_equiposCAL*12 + impuesto_gas*consumo_calefaccion + impuesto_electricidad*coste_variableCAL*consumo_calefaccion)*(1+factor_ivacal(tipo_calefaccion, provincia)/100) 
-    gasto_CALsinimp = coste_fijoCALreparto*12 + coste_variableCAL*consumo_calefaccion 
-
-    # REFRIGERACIÓN
-    zona_verano = zonaver(provincia, altitud) 
-    C1_verano = calcula_c1_verano(califE, zona_verano, tipo_vivienda) 
-    Io_Is_verano = io_is_verano(zona_verano,tipo_vivienda, C1_verano) 
-    demanda_RefRef = demanda_referenciarefrig(provincia, altitud, tipo_vivienda) 
-    demanda_CorregidaSignoRef = Io_Is_verano * demanda_RefRef * superficie * ratio_supverano/100 
-    demanda_CorregidaRef = fdemanda_corregidacal(demanda_CorregidaSignoRef) 
-    SEER = seer(zona_verano, califE) 
-    consumo_refrigeracion = demanda_CorregidaRef / SEER 
-
-   
-
-    # ELECTRICIDAD
+    #ELECTRICIDAD
+     # ELECTRICIDAD
 
     pot_contratada_punta_sinREF = potencia_poromision(Npax, tipo_calefaccion, 'punta') 
     pot_contratada_valle_sinREF = potencia_poromision(Npax, tipo_calefaccion, 'valle') 
@@ -151,7 +118,57 @@ def run_demo(inputs):
     gasto_ELEC_TOTAL_conREF = gasto_ELEC_noTERMICO_conREF + factor_electricidadacs(tipo_acs)*gasto_ACS + factor_electricidadcal(tipo_calefaccion)*gasto_CAL + gasto_REFRIGERACION 
     gasto_ELEC_TOTAL_sinREF = gasto_ELEC_noTERMICO_sinREF + factor_electricidadacs(tipo_acs)*gasto_ACS + factor_electricidadcal(tipo_calefaccion)*gasto_CAL 
     gasto_COMBUSTIBLE = (1 - factor_electricidadacs(tipo_acs))*gasto_ACS + (1 - factor_electricidadcal(tipo_calefaccion))*gasto_CAL
-    print (demanda_ACS)
+    
+    # CALEFACCIÓN
+    zona_invierno = zonainv(provincia, altitud) 
+    C1 = calcula_c1(califE, zona_invierno, tipo_vivienda) 
+    Io_Is = io_is(zona_invierno, tipo_vivienda, C1) 
+    demanda_RefCal = demanda_referenciacalefaccion(provincia, altitud, tipo_vivienda) 
+    demanda_CorregidaSigno = Io_Is * demanda_RefCal * superficie 
+    demanda_CorregidaCal = fdemanda_corregidacal(demanda_CorregidaSigno) 
+    HSPF = float(str(hspf(tipo_calefaccion, inst_calefaccion, zona_invierno, califE)).replace(",", "."))
+    
+    consumo_calefaccion = demanda_CorregidaCal / HSPF 
+    print("imprimiendo consumo calefaccion")
+    print(consumo_calefaccion)
+    print(type(consumo_calefaccion))
+    # TODAVIA NO HE DEFINIDO DEMANDA ACS
+    consumo_acs = demanda_ACS / ACS_SPF 
+    ##habia discrepancia entre consumo_ACS y consumo_acs
+    consumo_gnCAL = consumo_gas(tipo_calefaccion, tipo_acs, consumo_calefaccion, consumo_acs, inst_calefaccion, inst_acs, 'calefaccion') 
+    coste_fijoCAL = tarifa_suministro(inst_calefaccion, tipo_calefaccion, consumo_gnCAL, 'fijo', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
+    coste_fijoCALreparto = coste_fijocalreparto(tipo_calefaccion, inst_calefaccion, coste_fijoCAL) 
+    coste_variableCAL = tarifa_suministro(inst_calefaccion, tipo_calefaccion, consumo_gnCAL, 'variable', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
+    alquiler_equiposCAL = alquiler_equipos(tipo_calefaccion) 
+    impuesto_gas = impuesto_gas(tipo_calefaccion) 
+    impuesto_electricidad = impuesto_electricidad(tipo_calefaccion) 
+    gasto_CAL = (coste_fijoCALreparto*12 + coste_variableCAL*consumo_calefaccion + alquiler_equiposCAL*12 + impuesto_gas*consumo_calefaccion + impuesto_electricidad*coste_variableCAL*consumo_calefaccion)*(1+factor_ivacal(tipo_calefaccion, provincia)/100) 
+    gasto_CALsinimp = coste_fijoCALreparto*12 + coste_variableCAL*consumo_calefaccion 
+
+    #ACS consumos:
+    consumo_gnACS = consumo_gas(tipo_calefaccion, tipo_acs, consumo_calefaccion, consumo_ACS, inst_calefaccion, inst_acs, 'ACS') 
+    coste_fijoACS = tarifa_suministro(inst_acs, tipo_acs, consumo_gnACS, 'fijo', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
+    coste_fijoACSreparto = coste_fijoacsreparto(inst_acs, tipo_acs, inst_calefaccion, tipo_calefaccion, coste_fijoACS) 
+    coste_variableACS = tarifa_suministro(inst_acs, tipo_acs, consumo_gnACS, 'variable', provincia, zona_invierno, pot_contratada_punta_sinREF, pot_contratada_valle_sinREF) 
+    alquiler_equiposACS = alquiler_equiposacs(tipo_acs, inst_acs) 
+    impuesto_gasACS = impuesto_gas(tipo_acs) 
+    impuesto_electricidadACS = impuesto_electricidad(tipo_acs) 
+    gasto_ACS = (coste_fijoACSreparto*12 + coste_variableACS*consumo_ACS + alquiler_equiposACS*12 + impuesto_gasACS*consumo_ACS + impuesto_electricidadACS*coste_variableACS*consumo_ACS)*(1+factor_ivaacs(tipo_acs, provincia)/100) 
+    gasto_ACSsinimp = coste_fijoACSreparto*12 + coste_variableACS*consumo_ACS 
+
+    # REFRIGERACIÓN
+    zona_verano = zonaver(provincia, altitud) 
+    C1_verano = calcula_c1_verano(califE, zona_verano, tipo_vivienda) 
+    Io_Is_verano = io_is_verano(zona_verano,tipo_vivienda, C1_verano) 
+    demanda_RefRef = demanda_referenciarefrig(provincia, altitud, tipo_vivienda) 
+    demanda_CorregidaSignoRef = Io_Is_verano * demanda_RefRef * superficie * ratio_supverano/100 
+    demanda_CorregidaRef = fdemanda_corregidacal(demanda_CorregidaSignoRef) 
+    SEER = seer(zona_verano, califE) 
+    consumo_refrigeracion = demanda_CorregidaRef / SEER 
+
+   
+
+   
     return {
             # Demandas
             'demanda_CorregidaCal': demanda_CorregidaCal,
